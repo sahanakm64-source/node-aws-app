c601f4a390656b8a3e791dc5d7a966156dcc7a92
EC2 Default User <ec2-user@ip-172-31-34-128.ap-south-1.compute.internal>
2026-03-28 08:22:33 +0000

    update text

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) =>
  {
-	res.write('My App is live on AWS');
+	res.write('Hello CI/CD Pipeline');
         res.end();
 });
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
 	console.log('Server running on port 3000');
 });
