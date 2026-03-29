ca9bc45085ac30c214befc6d36a58e3164c2f43e
SAHANA K M <sahanakm64@gmail.com>
2026-03-29 22:09:52 +0530

    Change greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) =>
  {
-	res.write('Hello CI/CD Pipeline');
+	res.write('Hello World ');
         res.end();
 });
 server.listen(3000, '0.0.0.0', () => {
